81a94477a82a7a8605ac31c96a2cd41ce4081b06
Noel Rubio <noelrubio@air-de-noel.congresogto.gob.mx>
2017-09-22 13:27:55 -0500

    cambios de viernes 22

--- methods.rb
+++ methods.rb
@@ -10,11 +10,17 @@ def saludo(nombre="mundo")
   puts "hola #{nombre}"
 end
 
+#comentario
 def palindromo(palabra)
   if palabra == palabra.reverse
     return "palindromo"
   else
     return "no es palindromo"
   end
+end
 
+def facto
+  puts "numero? "
+  numero = gets.chomp.to_i
+  (1..numero).inject(:*) || 1
 end
